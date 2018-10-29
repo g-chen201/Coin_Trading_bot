622eeaa3d233840b50f73510f5a26abe3196fcd4
Jason <iverson201@qq.com>
2018-10-29 14:28:53 -0400

    first

--- Code/btc.py
+++ Code/btc.py
@@ -9,7 +9,7 @@ from Code.Trade import next_run_time, place_order, get_bitfinex_candle_data, aut
 from Code.Signals import signal_bolling
 #
 pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
-
+#
 """
 自动交易思路
 
